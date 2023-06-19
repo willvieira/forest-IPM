ab3b7250b553adad25884cf6fedaa0871a332bf8
Willian <willian.vieira@usherbrooke.ca>
2023-06-19 11:41:02 -0400

    new competition function to account for

--- R/BasalArea_competition.R
+++ R/BasalArea_competition.R
@@ -16,34 +16,47 @@ size_to_BAind <- function(
 }
 
 
-BAind_to_BAplot <- function(
-	BAind,
+size_to_BAplot <- function(
 	N,
 	plot_size
 ){
-	sum(BAind * N * 1e4/plot_size)
+	# get BAind
+	BAind <- size_to_BAind(N$meshpts)
+	sum(BAind * N$Nvec * 1e4/plot_size)
 }
 
 
+#' Function to compute BA competition in function of population size vectors
+#' N_intra: size distribution for focal species; output of `init_pop` function
+#' N_inter: size distribution of competition species
+#' If `N_inter` is NULL, BA_comp is from intraspecific competition. If `N_inter` is defined, then BA_comp is from interspecific competition
 size_to_BAcomp <- function(
-	mesh,
-	N,
+	N_intra,
+	N_inter = NULL,
 	plot_size
 ){
+	# Define weather intra or inter competition is used
+	if(is.null(N_inter)) {
+		N_focal <- N_intra
+	}else{
+		N_focal <- N_inter
+	}
+
 	# size to individual basal area in m2
-	baind = size_to_BAind(mesh)
+	baind = size_to_BAind(N_focal$meshpts)
 	
 	# basal in m2/ha corrected by the number of individuals
-	ba_N_plot = baind * N * 1e4/plot_size
-	
+	ba_N_plot = baind * N_focal$Nvec * 1e4/plot_size
+
 	sapply(
-		mesh,
+		N_intra$meshpts,
 		function(x)
-			sum(ba_N_plot[mesh > x])
+			sum(ba_N_plot[N_focal$meshpts > x])
 	)
 }
 
 
+# Deprecated function
 dbh_to_meshpts <- function(
 	dbh,
 	mesh
