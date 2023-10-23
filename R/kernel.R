3652123a64cc298ed86ea5f86aaf01f1d33cdbf2
Willian <willian.vieira@usherbrooke.ca>
2023-10-23 11:41:17 -0400

    new method to load parameters

--- R/kernel.R
+++ R/kernel.R
@@ -126,50 +126,55 @@ mkKernel = function(
 # function to get parameters for specific species_id
 #' sp: species_id
 #' method: either `mean` or `random`. `Mean` is the posterior mean and `random` is a single draw from the posterior mean
-#' path: default is set to 'data/parameters'
+#' model: name of specific demographic model. Can be either a character or a vector of size 3 with the name of the model for each of the [1] growth, [2] mort, or [3] recruitment model. Accepted values are `intcpt`, `plot`, `comp`, and `clim` that can be used in incremental form. Defaut set to the complete model: `intcpt_plot_comp_clim`.
+#' path: default is set to 'data/output_sim_processed'
 #' @export
-getPars_sp <- function(sp, method, path = NULL)
-{
-  pars_path <- ifelse(
-    is.null(path),
-    file.path('data', 'parameters'),
-    path
+getPars_sp <- function(
+  sp,
+  method,
+  model = 'intcpt_plot_comp_clim',
+  path = file.path('data', 'output_sim_processed')
+){
+  # deal for `model` parameter
+  if(length(model) == 1) {
+    model = rep(model, 3)
+  }else if(length(model) != 3) {
+    stop('`method` parameter must be a character of length 1 or 3.')
+  }
+
+  for(MD in model)
+    if(!MD %in% c('intcpt', 'intcpt_plot', 'intcpt_plot_comp', 'intcpt_plot_comp_clim')) {
+      stop(
+        paste0('model parameter should be one of the following character:\n', paste0(paste0('-', c('intcpt', 'intcpt_plot', 'intcpt_plot_comp', 'intcpt_plot_comp_clim')), collapse='\n'))
+      )
+    }
+  
+  # define full path to load parameters for each vital rate
+  pars_dir <- setNames(
+    paste0(
+      path, '/', c('growth', 'mort', 'recruit', 'sizeIngrowth'), '/', c(model, 'time_truc'), '/posterior_pop_', sp, '.RDS'
+    ),
+    c('growth', 'mort', 'rec', 'sizeIngrowth')
   )
-
-  files_to_load <- dir(
-    pars_path,
-    pattern = sp,
-    full.names = TRUE
-  )
-
-  # remove  random effect files
-   files_to_load <- files_to_load[
-    grep('meanRandomEffect', files_to_load, invert = TRUE)
-   ]
-
-  # read pars
-  pars <- map(
-      setNames(
-        files_to_load,
-        gsub(
-          paste0(pars_path, '/|_|', sp, '|.csv'),
-          '',
-          files_to_load
-        )
-      ),
-      ~ read_csv(.x, progress = FALSE, show_col_types = FALSE)
-    )
 
   # export pars
   if(method == 'mean') {
-    pars |>
-      map(~apply(.x, 2, mean))
-  }else if(method == 'random') {
-    pars |>
+    pars_dir |>
       map(
-        ~ .x[sample(1:nrow(.x), 1), ] |>
-          pivot_longer(cols = everything()) |>
-          deframe()
+        ~ readRDS(.x) |>
+          filter(par != 'lp__') |>
+          group_by(par) |>
+          reframe(value = mean(value)) |>
+          pivot_wider(names_from = 'par')
+      )
+  }else if(method == 'random') {
+    pars_dir |>
+      map(
+        ~ readRDS(.x) |>
+          filter(par != 'lp__') |>
+          filter(iter == sample(1:4000, 1)) |>
+          select(!iter) |>
+          pivot_wider(names_from = 'par')          
       )
   }else{
     stop('`param_method` parameter must be either `mean` or `random`')
